78a09750c6e8a034c84488d9947e3cf7c3bad572
Sean H <DjPigPigstar@gmail.com>
2026-01-13 18:33:19 -0800

    HierarchyPanel start

---
+++ AvaloniaInterface/ViewModels/HierarchyPanel.cs
@@ -0,0 +1,20 @@
+﻿using Avalonia.Controls;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OpenglAvaloniaTest.ViewModels;
+
+public class HierarchyPanel : UserControl
+{
+    public HierarchyPanel()
+    {
+        InitializeComponent();
+    }
+
+    private void InitializeComponent()
+    {
+    }
+}
