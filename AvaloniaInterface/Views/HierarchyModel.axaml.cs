117d20b7ebbd97ce79db692b5f412d20e77f78c8
Sean H <DjPigPigstar@gmail.com>
2026-01-15 20:23:06 -0800

    Hierarchy preview

--- AvaloniaInterface/Views/HierarchyModel.axaml.cs
+++ AvaloniaInterface/Views/HierarchyModel.axaml.cs
@@ -2,14 +2,27 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Input;
 using Avalonia.Markup.Xaml;
+using Models;
+using System;
 
 namespace OpenglAvaloniaTest;
 
 public partial class HierarchyModel : UserControl
 {
+
+    public Model? Model;
+
     public HierarchyModel()
     {
         InitializeComponent();
+        ReadModelData();
+    }
+    public void ReadModelData()
+    {
+        
+        if (Model == null) return;
+        Console.WriteLine("Fuck");
+        NameTextbox.Text = Model.ObjectName;
     }
 
     private void OnNameKeyDown(object? sender, Avalonia.Input.KeyEventArgs e)
--- AvaloniaInterface/Views/HierarchyView.axaml.cs
+++ AvaloniaInterface/Views/HierarchyView.axaml.cs
@@ -2,15 +2,35 @@ using Avalonia;
 using Avalonia.Controls;
 using Avalonia.Markup.Xaml;
 using Models;
+using System;
 using System.Collections.ObjectModel;
 
 namespace OpenglAvaloniaTest.ViewModels;
 
-public partial class HierarchyView : UserControl
+public partial class HierarchyView : UserControl, IDisposable
 {
+
     public HierarchyView()
     {
         InitializeComponent();
-        HierarchyStack.Children.Add(new HierarchyModel { });
+        foreach(Model model in SceneHierarchy.Instance.Models)
+        {
+            OnModelAdded(model);
+        }
+        SceneHierarchy.Instance.OnModelAdded += OnModelAdded;
+    }
+
+    private void OnModelAdded(Model model)
+    {
+        HierarchyModel modelView = new HierarchyModel { Model = model };
+        HierarchyStack.Children.Add(modelView);
+        modelView.ReadModelData();
+    }
+
+    ~HierarchyView() => Dispose();
+
+    public void Dispose()
+    {
+        SceneHierarchy.Instance.OnModelAdded -= OnModelAdded;
     }
 }
