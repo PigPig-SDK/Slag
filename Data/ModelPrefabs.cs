76c829e15190e4c6e402479c3a4b0521497eb827
Sean H <DjPigPigstar@gmail.com>
2026-02-09 15:52:50 -0800

    Model selection prefab

--- Data/ModelPrefabs.cs
+++ Data/ModelPrefabs.cs
@@ -323,4 +323,12 @@ public static partial class ModelPrefabs
 
         return triad;
     }
+
+    public static Model SelectionInstance()
+    {
+        Model selectionModel = new();
+        selectionModel.ObjectName = "SelectionInstance";
+        selectionModel.AddComponent<SelectionMeshInstance>(new SelectionMeshInstance());
+        return selectionModel;
+    }
 }
