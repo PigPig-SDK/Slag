7760cc2ea462a0cb33f11c59bc8146d43e788fac
Sean H <DjPigPigstar@gmail.com>
2026-03-28 21:31:42 -0700

    UI overhaul

---
+++ AvaloniaInterface/ViewModels/Converters.cs
@@ -0,0 +1,9 @@
+﻿using Avalonia.Data.Converters;
+
+namespace UI.ViewModels;
+
+public static class Converters
+{
+    public static readonly FuncValueConverter<double, bool> WiderThan60 =
+        new FuncValueConverter<double, bool>(w => w > 60);
+}
