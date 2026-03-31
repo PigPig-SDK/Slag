5f732906947dfd19ce36044cdf6db441e2137cc3
Sean H <DjPigPigstar@gmail.com>
2026-03-30 19:35:33 -0700

    Merge command backing

---
+++ UI/Commands/MergeCommand.cs
@@ -0,0 +1,29 @@
+﻿using Avalonia.Input;
+using Core;
+using System;
+using System.Linq;
+using UI.ViewModels;
+
+namespace UI.Commands;
+
+public class MergeCommand : MementoCommand
+{
+    public ICommand? Next { get; set; }
+
+    public override string Name => throw new NotImplementedException();
+    public override string Description => throw new NotImplementedException();
+    public override bool ShowUpOToolbar => false;
+
+    public override CommandState Execute((KeyEventArgs? keyEvent, PointerEventArgs? mouseEvent, CommandInfo info) args)
+    {
+        if (SelectionManager.Instance.CurrentModel is null) return CommandState.Discard;
+
+        SelectionComponent? selection = SelectionManager.Instance.GetSelectionComponent();
+        if (selection is null) return CommandState.Discard;
+
+        
+
+        return CommandState.Finished;
+    }
+
+}
